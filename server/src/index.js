89790403727422fe62ec60afc3e79f5fbb9aca8c
soulfulDreamer <kazishahhamza@outlook.com>
2025-03-18 14:14:30 +0600

    mongodb setup

--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,25 @@
 import express from "express";
 import cors from "cors";
+import mongoose from "mongoose";
 
 const app = express();
 
-
+app.use(express.json());
 app.use(cors());
+
+const connectDB = async () => {
+  try {
+    console.log("Connecting to MongoDB...");
+    const conn = await mongoose.connect(
+      "mongodb+srv://kazishahhamza:u9i5RzWFxlIbYxCf@cluster0.2rnwq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    );
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`MongoDB Connection Error: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+connectDB();
+
 app.listen(3001, () => console.log("Server running on port 3001"));
