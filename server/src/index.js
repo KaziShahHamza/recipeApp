dd7cdff8e53b8849563e933972772260bdd7e87e
soulfulDreamer <kazishahhamza@outlook.com>
2025-03-19 21:01:40 +0600

    Add schema model

--- server/src/index.js
+++ server/src/index.js
@@ -1,15 +1,19 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
-
+import { userRouter } from "./routes/Users.js";
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
+
+app.use("/auth", userRouter);
+
 const connectDB = async () => {
+
   try {
-    console.log("Connecting to MongoDB..");
+    console.log("Connecting to MongoDB...");
     const conn = await mongoose.connect(
       "mongodb+srv://kazishahhamza:u9i5RzWFxlIbYxCf@cluster0.2rnwq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     );
