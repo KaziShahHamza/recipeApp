02e5ec87cb220b6a63ff336d5ec3ea9e114b7b35
soulfulDreamer <kazishahhamza@outlook.com>
2025-03-26 18:08:20 +0600

    Fix register api route

--- server/src/routes/UserRouter.js
+++ server/src/routes/UserRouter.js
@@ -1,18 +1,24 @@
 import express from "express";
 import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 import UserModel from "../models/UserModel.js";
 
 const router = express.Router();
 
 router.post("/register", async (req, res) => {
-  const { username, pass } = req.body;
+  const { username, password } = req.body;
 
-  const person = await UserModel.findOne({ username: username});
-  if (person) {
-    res.json(person);
-  } else {
-    res.json("No user found");
-  }
+  const user = await UserModel.findOne({ username });
+  if (user) {
+    res.json({message: "user already exists"});
+  } 
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const newUser = new UserModel({username, password: hashedPassword});
+  await newUser.save();
+
+  res.json({message: "user registered successfully"});
 });
 
 export { router as userRouter };
