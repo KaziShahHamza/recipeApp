f92b71e7433a4f8d2e8f6296335e92dde29c1a57
soulfulDreamer <kazishahhamza@outlook.com>
2025-03-27 19:15:36 +0600

    Add client register function

---
+++ client/src/components/Login.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+import Form from "./Form";
+
+const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <Form
+      username={username}
+      setUsername={setUsername}
+      password={password}
+      setPassword={setPassword}
+      label="Login"
+    />
+  );
+};
+
+export default Login;
---
+++ client/src/components/Register.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import Form from "./Form";
+import axios from "axios";
+
+const Register = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    console.log("entered handleSubmit");
+    try {
+      await axios.post("http://localhost:3001/auth/register", {
+        username,
+        password,
+      });
+      console.log("registration completed");
+      alert("Registration completed, now login to continue");
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <Form
+      username={username}
+      setUsername={setUsername}
+      password={password}
+      setPassword={setPassword}
+      label="Register"
+      onSubmit={onSubmit}
+    />
+  );
+};
+
+export default Register;
