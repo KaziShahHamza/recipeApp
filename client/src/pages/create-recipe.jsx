6ec89d7758727bb4268782d2e29e43280962ea28
soulfulDreamer <kazishahhamza@outlook.com>
2025-03-29 17:12:48 +0600

    Add create recipe function

--- client/src/pages/create-recipe.jsx
+++ client/src/pages/create-recipe.jsx
@@ -1,7 +1,94 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import { useGetUserID } from "../hooks/useGetUserID";
 
 const CreateRecipe = () => {
-  return <div>CreateRecipe</div>;
+  const userID = useGetUserID();
+  const [recipe, setRecipe] = useState({
+    name: "",
+    ingredients: [],
+    instructions: "",
+    imageUrl: "",
+    cookingTime: 0,
+    userOwner: userID,
+  });
+
+  const handleSubmit = (e) => {
+    const { name, value } = e.target;
+    setRecipe({ ...recipe, [name]: value });
+  };
+
+  const handleIngredientSubmit = (e, idx) => {
+    const { value } = e.target;
+    const ingredients = recipe.ingredients;
+    ingredients[idx] = value;
+    setRecipe({ ...recipe, ingredients });
+  };
+
+  const addIngredient = () => {
+    setRecipe({ ...recipe, ingredients: [...recipe.ingredients, ""] });
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post("http://localhost:3001/recipes", recipe);
+      console.log("Recipe Created");
+      alert("Recipe Created");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  console.log(recipe);
+  return (
+    <div className="create-recipe">
+      <h2>Create Recipe</h2>
+      <form onSubmit={onSubmit}>
+        <label htmlFor="name">Name</label>
+        <input type="text" id="name" name="name" onChange={handleSubmit} />
+
+        <label htmlFor="ingredients">Ingredients</label>
+        {recipe.ingredients.map((ingredient, idx) => (
+          <input
+            key={idx}
+            type="text"
+            name="ingredients"
+            value={ingredient}
+            onChange={(e) => handleIngredientSubmit(e, idx)}
+          />
+        ))}
+        <button onClick={addIngredient} type="button">
+          Add Ingredient
+        </button>
+
+        <label htmlFor="instructions">Instructions</label>
+        <textarea
+          id="instructions"
+          name="instructions"
+          onChange={handleSubmit}
+        ></textarea>
+
+        <label htmlFor="imageUrl">Image URL</label>
+        <input
+          type="text"
+          id="imageUrl"
+          name="imageUrl"
+          onChange={handleSubmit}
+        />
+
+        <label htmlFor="cookingTime">CookingTime (Minutes)</label>
+        <input
+          type="text"
+          id="cookingTime"
+          name="cookingTime"
+          onChange={handleSubmit}
+        />
+
+        <button type="submit">Create Recipe</button>
+      </form>
+    </div>
+  );
 };
 
 export default CreateRecipe;
